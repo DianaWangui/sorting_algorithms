52ca5557edbbddba82f24d0daca971a2edcf6f6f
DianaWangui <dianawanguikt048@gmail.com>
2023-12-15 22:53:40 +0300

    checkers failing, printing all lines

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t j, i, temp;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
@@ -19,6 +22,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
