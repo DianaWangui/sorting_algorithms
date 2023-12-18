bdcdf6f9afc7b2f056ca0b4ad5b384330ba3f6ee
bo1atit0 <shorungbebolatito@gmail.com>
2023-12-18 10:54:03 -0800

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -32,16 +32,14 @@ void swap(int *a, int *b)
 void lomuto_quick_sort(int *array, size_t size, int start, int end)
 {
 
-  int pivotIndex;
-  
-	/*if (start - end > 0)*/
+int pivotIndex;
+
+/*if (start - end > 0)*/
 if (start < end)
-	{
-		
+{
+pivotIndex = partition(array, size, start, end);
 
-		pivotIndex = partition(array, size, start, end);
-
-		/* Use recursion to sort subarrays on the right and left side of the pivot */
+/* Use recursion to sort subarrays on the right and left side of the pivot */
 		lomuto_quick_sort(array, size, start, pivotIndex - 1);
 		lomuto_quick_sort(array, size, pivotIndex + 1, end);
 	}
@@ -69,36 +67,36 @@ void quick_sort(int *array, size_t size)
  * its correct position.
  * @array: The array to be partitioned.
  * @size: size of the array.
- * @start: The starting index of the subarray.
- * @end: The ending index of the subarray.
+ * @left: The starting index of the subarray.
+ * @right: The ending index of the subarray.
  *
  * Return: The index of the pivot.
  */
 
 int partition(int *array, size_t size, int left, int right)
 {
-  int great;
-  int less;
+int great;
+int less;
 	int *pivot = array + right;
-	
-  
-  for (great = less = left; less < right; less++) /* Iterate from the first-*/
-					 /*-element to the second-to-last element */
+
+
+	for (great = less = left; less < right; less++) /* Iterate from the first-*/
+		/*-element to the second-to-last element */
 	{
 		if (array[less] < *pivot)
 		{
-      if (great < less)
-      {
-			swap(array + less, array + great);
-      print_array(array, size);
+			if (great < less)
+			{
+				swap(array + less, array + great);
+				print_array(array, size);
+			}
+			great++;
 		}
-    great++;
 	}
-  }
-if (array[great] > *pivot)
-{
-	swap(array + great, pivot);
-	print_array(array, size);
-}
-return (great);
+	if (array[great] > *pivot)
+	{
+		swap(array + great, pivot);
+		print_array(array, size);
+	}
+	return (great);
 }
