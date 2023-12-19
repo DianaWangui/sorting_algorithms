af66fcb5ebef189d05e1ae09051a95213ef28566
DianaWangui <dianawanguikt048@gmail.com>
2023-12-19 10:56:45 +0300

    betty error

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,8 +11,8 @@ void shell_sort(int *array, size_t size)
 	size_t div = 3;
 	int temp;
 
-	for (interval = 1 ; interval <= size / div; interval = interval * div + 1);
-
+	for (interval = 1 ; interval <= size / div; interval = interval * div + 1)
+		;
 	while (interval > 0)
 	{
 		for (i = interval; i < size; i++)
